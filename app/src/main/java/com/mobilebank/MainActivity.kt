50bb1788b2a719d399f8476aa20b49e4ce2f937b
fidanhasanguliyeva <004724@ibar.int>
2024-02-21 16:31:24 +0400

    navigation

--- app/src/main/java/com/mobilebank/MainActivity.kt
+++ app/src/main/java/com/mobilebank/MainActivity.kt
@@ -2,11 +2,38 @@ package com.mobilebank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
+import com.mobilebank.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+    private lateinit var navGraph: NavGraph
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setUpNavigationController()
     }
+
+    fun setUpNavigationController() {
+        val host: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+                ?: return
+
+        navController = host.navController
+        navGraph = navController.navInflater.inflate(R.navigation.main_navigation)
+
+        navController.setGraph(navGraph, intent.extras)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+        }
+    }
+
+
 }
